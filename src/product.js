9c284fc4c3be420eb2760618cc55493f9363a7a5
cyrus devnomad <cyrus.devnomad@gmail.com>
2022-01-21 11:48:30 +0100

    test

--- src/product.js
+++ src/product.js
@@ -1,9 +1,9 @@
 export class Product {
-  constructor({id, name, type, color, price}) {
+  constructor({id, name, type, color, pricexyz}) {
     this.id = id
     this.name = name
     this.type = type
     this.color = color
-    this.price = price;
+    this.pricexyz = pricexyz;
   }
 }
